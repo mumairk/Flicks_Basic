9bcf9743ec0aa8ea67a13b24de535522938b0817
Barbara Ristau <ristaub@gmail.com>
2017-01-12 15:08:44 -0800

    added logos, fade in images

--- Flicks_Basic/MoviesViewController.swift
+++ Flicks_Basic/MoviesViewController.swift
@@ -11,52 +11,54 @@ import AFNetworking
 import MBProgressHUD
 
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
   
+  
+  @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var collectionView: UICollectionView!
   
-  var currentView: UIView!
+  @IBOutlet weak var toggleViewButton: UIButton!
   
+  var currentView: UIView!
   var movies: [NSDictionary]?
+  var filteredMovies: [NSDictionary]?
+  var imageUrl: NSURL!
+  var collectionIsFirstView: Bool = true
   
   let apiKey = "08626c78c1c24c6f0e9912f59264d957"
   let urlString = "https://api.themoviedb.org/3/movie/now_playing?api_key="
   
+  let refreshControl = UIRefreshControl()
   
   override func viewDidLoad() {
+    
     super.viewDidLoad()
-    
-    let refreshControl = UIRefreshControl()
-    refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
-    
-    // add refresh control to tableview
-    tableView.insertSubview(refreshControl, at: 0)
-    
-    tableView.dataSource = self
-    tableView.delegate = self
-    
-    currentView = self.tableView
-    
-    collectionView.insertSubview(refreshControl, at: 0)
     
     collectionView.dataSource = self
     collectionView.delegate = self
+    tableView.dataSource = self
+    tableView.delegate = self
+    searchBar.delegate = self
+    filteredMovies = movies
     
-    loadDataFromNetwork()
+    //  toggleViewButton.titleLabel?.text = "Switch"
+    
+   loadDataFromNetwork()
+    
+    if collectionIsFirstView {
+      loadCollectionView()
+    }
     
   }
   
   
+  // MARK: - TABLEVIEW
+  
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-    if let movies = movies {
-      return movies.count
-    } else {
-      return 0
-    }
-    
+    return filteredMovies?.count ?? 0
   }
   
   
@@ -64,28 +66,104 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
     
-    let movie = movies![indexPath.row]
+    let movie = filteredMovies![indexPath.row]
     let title = movie["title"] as! String
     let overview = movie["overview"] as! String
     
     let baseUrl = "http://image.tmdb.org/t/p/w342"
-    let posterPath = movie["poster_path"] as! String
     
-    let imageUrl = NSURL(string: baseUrl + posterPath)
-    cell.posterView.setImageWith(imageUrl as! URL)
+    if let posterPath = movie["poster_path"] as? String {
+      
+      imageUrl = NSURL(string: baseUrl + posterPath)
+      fadeInImageRequest(poster: cell.posterView)
+      
+    }
     
     
     cell.titleLabel.text = title
     cell.overviewLabel.text = overview
-    
-    //cell.textLabel?.text = "row \(indexPath.row)"
-    //print("row\(indexPath.row)")
     
     return cell
     
     
   }
   
+  // MARK: - TOGGLE VIEW
+  
+  @IBAction func toggleView(_ sender: Any) {
+    
+    print("Pressed toggle button")
+    
+    if currentView == self.tableView {
+      
+      loadCollectionView()
+      self.reloadData()
+      print("Loading collection view")
+    }
+      
+    else if currentView == self.collectionView {
+      
+      loadTableView()
+      self.reloadData()
+      currentView = self.tableView
+      
+    }
+    
+  }
+  
+  func loadCollectionView() {
+    
+    toggleViewButton.setImage(#imageLiteral(resourceName: "grid48"), for: .normal)
+    
+    UIView.transition(from: self.tableView, to: self.collectionView, duration: 0, options: .showHideTransitionViews, completion: nil)
+    refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+    collectionView.insertSubview(refreshControl, at: 0)
+    currentView = self.collectionView
+    
+  }
+  
+  func loadTableView() {
+    
+    toggleViewButton.setImage(#imageLiteral(resourceName: "list48"), for: .normal)
+
+    
+    UIView.transition(from: self.collectionView, to: self.tableView, duration: 0, options: .showHideTransitionViews, completion: nil)
+    refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
+    tableView.insertSubview(refreshControl, at: 0)
+    currentView = self.tableView
+    
+  }
+  
+  
+  // MARK: - COLLECTION VIEW
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    return filteredMovies?.count ?? 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+    
+    let movie = filteredMovies![indexPath.item]
+    
+    let title = movie["title"] as! String
+    cell.titleLabel.text = title
+    
+    
+    let baseUrl = "http://image.tmdb.org/t/p/w342"
+    
+    if let posterPath = movie["poster_path"] as? String {
+      
+      imageUrl = NSURL(string: baseUrl + posterPath)
+      fadeInImageRequest(poster: cell.posterImageView)
+      
+    }
+    
+    return cell
+    
+  }
+  
+
   
   // MARK: - NETWORK REQUEST
   
@@ -110,9 +188,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
           MBProgressHUD.hide(for: self.view, animated: true)
           
           self.movies = dataDictionary["results"] as? [NSDictionary]
-          
+          self.filteredMovies = self.movies
+          self.refreshControl.endRefreshing()
           self.reloadData()
-          
           
         }
       } else {
@@ -120,45 +198,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
       }
     }
     task.resume()
-    
   }
   
-  
-  // makes a network request to get updated data
-  // updates the tableview with the new data
-  // hides the refresh control
-  
-  
-  func refreshControlAction(refreshControl: UIRefreshControl){
+  func reloadData(){
     
-    let url = URL(string: urlString + apiKey)!
-    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-    
-    let session = URLSession(configuration: URLSessionConfiguration.default,
-                             delegate: nil, delegateQueue: OperationQueue.main
-    )
-    
-    let task : URLSessionDataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
-      
-      // use new data to update the data source
-      
-      if let data = data {
-        if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-          
-          self.movies = dataDictionary["results"] as? [NSDictionary]
-          
-          // reload the table view now that there is new data
-          // self.tableView.reloadData()
-          self.reloadData()
-          
-          // tell refresh control to stop spinning
-          refreshControl.endRefreshing()
-          
-        }
-      }
-      
-    });
-    task.resume()
+    self.tableView.reloadData()
+    self.collectionView.reloadData()
     
   }
   
@@ -170,72 +215,73 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
   }
   
-  
-  
-  @IBAction func toggleView(_ sender: Any) {
+  func fadeInImageRequest(poster: UIImageView) {
     
-    print("Pressed toggle button")
     
-    if currentView == self.tableView {
+    let imageRequest = URLRequest(url: imageUrl as URL)
+    
+    poster.setImageWith(imageRequest as URLRequest, placeholderImage: nil, success: {( imageRequest, imageResponse, image) -> Void in
       
-      UIView.transition(from: self.tableView, to: self.collectionView, duration: 0.25, options: .showHideTransitionViews, completion: nil)
-      loadDataFromNetwork()
-      self.reloadData()
-      currentView = self.collectionView
-      print("Loading collection view")
-    }
+      // image response will be nil if image is cached
       
-    else if currentView == self.collectionView {
-      
-      UIView.transition(from: self.collectionView, to: self.tableView, duration: 0.25, options: .showHideTransitionViews, completion: nil)
-      self.reloadData()
-      currentView = self.tableView
-      print("Loading table view")
-      
-    }
+      if imageResponse != nil {
+        print("Image was NOT cached, fade in image")
+        poster.alpha = 0.0
+        
+        
+        poster.image = image
+        UIView.animate(withDuration: 2.0, animations: { () -> Void in
+          poster.alpha = 3.0
+        })
+      } else {
+        print ("Image was cached so just update the image")
+        poster.image = image
+      }
+    },
+                        failure: {(imageRequest, imageResponse, error) -> Void in
+                          
+                          print("Failure to get image")
+                          
+    })
     
   }
   
   
-  func reloadData(){
+  // MARK: - REFRESH CONTROL
+  
+  func refreshControlAction(refreshControl: UIRefreshControl){
     
-    
-    if currentView == self.tableView {
-      self.tableView.reloadData()
-    }  else if currentView == self.collectionView {
-      self.collectionView.reloadData()
-    }
+    loadDataFromNetwork()
     
   }
   
+  // MARK: - SEARCH BAR
   
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    if let movies = movies {
-      return movies.count
-    } else {
-      return 0
-    }
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    filteredMovies = searchText.isEmpty ? movies : movies?.filter({(movie: NSDictionary) -> Bool in
+      return (movie["title"] as! String).range(of: searchText, options: .caseInsensitive) != nil
+    })
+    
+    reloadData()
   }
   
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
-    
-    let movie = movies![indexPath.item]
-
-    let title = movie["title"] as! String
-    cell.titleLabel.text = title
-
-    
-    let baseUrl = "http://image.tmdb.org/t/p/w342"
-    let posterPath = movie["poster_path"] as! String
-    
-    if let imageUrl = NSURL(string: baseUrl + posterPath) {
-    cell.posterImageView.setImageWith(imageUrl as URL)
-    }
- 
-    return cell
-    
+  func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+    self.searchBar.showsCancelButton = true
   }
+  
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.showsCancelButton = false
+    searchBar.text = ""
+    searchBar.resignFirstResponder()
+    loadDataFromNetwork()
+  }
+  
+  
+  
+  
+  
+  
+  
   
   
   /*
